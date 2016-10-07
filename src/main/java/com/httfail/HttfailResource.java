f958ebccdfd1cbd23433e1296e1702712da6026b
Daniel Reynaud <reynaud.daniel@gmail.com>
2016-10-06 18:10:22 -0700

    Registering for POST, PUT, DELETE, HEAD, OPTIONS...
    Not super elegant but does the job

--- src/main/java/com/httfail/HttfailResource.java
+++ src/main/java/com/httfail/HttfailResource.java
@@ -2,10 +2,7 @@ package com.httfail;
 
 import com.codahale.metrics.annotation.Timed;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -20,15 +17,54 @@ public class HttfailResource {
         this.defaultReason = defaultReason;
     }
 
-    @GET
-    public Response sayHello() {
-        return Response.status(defaultStatus).entity(defaultReason).build();
+    private Response respond(Integer status, String reason) {
+        return Response.status(status).entity(reason).build();
     }
 
     @GET
-    @Timed
+    public Response get() { return respond(defaultStatus, defaultReason); }
+
+    @GET
     @Path("/{http-status}")
-    public Response sayHello(@PathParam("http-status") Integer httpStatus) {
-        return Response.status(httpStatus).entity(defaultReason).build();
-    }
+    public Response get(@PathParam("http-status") Integer httpStatus) { return respond(httpStatus, defaultReason); }
+
+
+    @POST
+    public Response post() { return respond(defaultStatus, defaultReason); }
+
+    @POST
+    @Path("/{http-status}")
+    public Response post(@PathParam("http-status") Integer httpStatus) { return respond(httpStatus, defaultReason); }
+
+
+    @PUT
+    public Response put() { return respond(defaultStatus, defaultReason); }
+
+    @PUT
+    @Path("/{http-status}")
+    public Response put(@PathParam("http-status") Integer httpStatus) { return respond(httpStatus, defaultReason); }
+
+
+    @DELETE
+    public Response delete() { return respond(defaultStatus, defaultReason); }
+
+    @DELETE
+    @Path("/{http-status}")
+    public Response delete(@PathParam("http-status") Integer httpStatus) { return respond(httpStatus, defaultReason); }
+
+
+    @HEAD
+    public Response head() { return respond(defaultStatus, defaultReason); }
+
+    @HEAD
+    @Path("/{http-status}")
+    public Response head(@PathParam("http-status") Integer httpStatus) { return respond(httpStatus, defaultReason); }
+
+
+    @OPTIONS
+    public Response options() { return respond(defaultStatus, defaultReason); }
+
+    @OPTIONS
+    @Path("/{http-status}")
+    public Response options(@PathParam("http-status") Integer httpStatus) { return respond(httpStatus, defaultReason); }
 }
